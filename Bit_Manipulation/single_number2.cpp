6e369c161ad2435d089c5cff11c18468133067a0
Ariv-lamba <108919123+Ariv-lamba@users.noreply.github.com>
2025-08-14 10:19:35 +0530

    Update single_number2.cpp

--- Bit_Manipulation/single_number2.cpp
+++ Bit_Manipulation/single_number2.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i =0; i<32; i++){
+            int sum = 0;
+            for(int j =0; j<nums.size(); j++){
+                int x = nums[j]>>i;
+                x = x&1;
+                if(x==1){
+                    sum++;
+                    sum = sum%3;
+                }
+            }
+            sum= sum<<i;
+            if(sum !=0){
+                ans = ans|sum;
+            }
+        }
+        return ans;
+    }
+};
